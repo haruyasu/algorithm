aed86075f7e16dd3a893ccf0561fafe33835353c
haruyasu <harukun2002@gmail.com>
2017-10-30 21:32:49 -0700

    basic slice

---
+++ slice.py
@@ -0,0 +1,19 @@
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print a[2]
+# n m-1
+# a[n:m]
+
+print a[1:4]
+print a[:3]
+print a[7:]
+
+print a[-1]
+print a[-2:]
+
+print a[::3]
+print a[2:7:3]
+print a[::-3]
+
+# reverse
+print a[::-1]
